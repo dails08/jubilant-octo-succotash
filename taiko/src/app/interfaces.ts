52f4da7943844ca99648c91d7fe5bf84c6b99072
Chris Dailey <christopher.dailey@gmai.com>
2025-02-13 08:48:18 -0500

    Initial commit

---
+++ taiko/src/app/interfaces.ts
@@ -0,0 +1,12 @@
+export interface GrammarPoint {
+    chapter: number,
+    point: number,
+    grammar: string
+};
+
+export interface VocabularyPoint {
+    chapter: number,
+    kanji: string,
+    reading: string,
+    meaning: string,
+}
