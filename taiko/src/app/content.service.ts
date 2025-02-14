acec9329f60ce4f93314a70063806e53e391fb7b
Chris Dailey <christopher.dailey@gmail.com>
2025-02-13 19:49:28 -0500

    Adds list of chapters and iterating logic in reactive form.

--- taiko/src/app/content.service.ts
+++ taiko/src/app/content.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { GrammarPoint, VocabularyPoint } from "./interfaces";
+import * as grammarData from "../assets/grammar.json";
+import * as vocabData from "../assets/vocab.json";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
-  constructor() { }
+  constructor() {
+
+    this.vocabularyList = vocabData.map(elem => {return elem as VocabularyPoint})
+    this.grammarList = grammarData.map(elem => {
+      return {
+        chapter: parseInt(elem.chapter) ,
+        point: parseInt(elem.point),
+        grammar: elem.grammar
+      };
+    });
+
+    console.log(vocabData);
+   }
+
+   vocabularyList: VocabularyPoint[];
+   grammarList: GrammarPoint[];
+
+   getGrammarByChapterList(chapters: number[]): GrammarPoint[] {
+    return this.grammarList.filter(elem => {return chapters.includes(elem.chapter)});
+   }
+
+   getVocabularyByChapterList(chapters: number[]): VocabularyPoint[] {
+    return this.vocabularyList.filter(elem => {return chapters.includes(elem.chapter)});
+   }
 }
--- taiko/src/app/settings/settings.component.ts
+++ taiko/src/app/settings/settings.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
-
+import { ReactiveFormsModule, FormControl, FormGroup, FormArray } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ContentService } from '../content.service';
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
 
+  constructor(
+    content: ContentService
+  ) {
+    this.chaptersForm = new FormGroup({
+      chapterOptions: new FormArray(this.chapters.map((val, ix, arr) => {
+        return new FormControl('');
+      }))
+    });
+  }
+  chapters = [
+    "日本の地理",
+    "日本語のスピーチスタイル",
+    "日本のテクノロジー",
+    "日本のスポーツ",
+    "日本の食べ物",
+    "日本人と宗教",
+    "日本のポップカルチャー",
+    "日本の伝統芸能",
+    "日本の教育",
+    "日本の便利な店",
+    "日本の歴史",
+    "日本の伝統工芸",
+    "日本人の自然",
+    "日本の政治",
+    "世界と私の国の未来"
+  ];
+
+  chaptersForm: FormGroup;
+
+
+
+
+  get chapterOptions() {
+    return this.chaptersForm.get("chapterOptions") as FormArray;
+  }
 }
